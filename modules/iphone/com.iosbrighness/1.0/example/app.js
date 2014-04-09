b8c0e2d94140c33b823a2e80f43ae8d2e19376f3
Jayesh Joshi <jnj.idr@gmail.com>
2014-04-09 11:41:21 +0530

    device brigntness
    device brigntness

---
+++ modules/iphone/com.iosbrighness/1.0/example/app.js
@@ -0,0 +1,25 @@
+var win = Titanium.UI.createWindow();
+
+var mod = require('com.iosbrighness');
+Titanium.API.info("Jayesh joshi module loaded : " + mod);
+var Slidecontrol = Titanium.UI.createSlider({
+    top : 10,
+    width : '100%',
+    height : 50
+});
+Titanium.API.info("device brighness value for slider : " + mod.getBrightNeesDevice);
+//When Application launch at first
+var sldiervalue = mod.getBrightNeesDevice;
+Slidecontrol.value = sldiervalue;
+Titanium.App.addEventListener('resume', function(e) {
+    // change broghness from devic so that will adjust over here
+    var sldiervalue = mod.getBrightNeesDevice;
+    Slidecontrol.value = sldiervalue;
+});
+
+Slidecontrol.addEventListener('change', function(e) {
+    Titanium.API.info("slider value for device brighness:" + e.value);
+    mod.setBrightnessDevice(e.value);
+});
+win.add(Slidecontrol);
+win.open();
